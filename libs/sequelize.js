1eb80580583cef0dbb6bdf9b72e6a386ef9a80fc
Cesar <craupesantez@gmail.com>
2022-06-25 00:37:44 -0500

    correccion de la sincronizacion

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,27 +2,17 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const  setupModels  = require('./../db/models');
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI =`postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI =`postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const options ={
+const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
-  logging: config.isProd ? false : true,
-}
-
-if(config.isProd) {
-  options.dialectOptions ={
-    ssl :{
-      rejectUnauthorized: false
-    }
-  }
-}
-
-const sequelize = new Sequelize(config.dbUrl, options);
+  logging: true,
+})
 
 setupModels(sequelize);
 
-  // sequelize.sync();
+  sequelize.sync();
 
 module.exports = sequelize;
